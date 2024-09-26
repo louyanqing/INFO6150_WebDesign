348db2246491391152b4d5edf34d52d630c069c3
louyanqing <lou.yan@northeastern.edu>
2024-09-25 20:18:35 -0400

    Interactive table manipulation using JS

---
+++ week3_assmt_table/README.md
@@ -0,0 +1,31 @@
+### Assignment 3
+
+1.When Page loads Display your Full Name and NUID
+
+2.Table should not be expanded when page loads
+
+3.Submit button to be disabled and grayed out and it should not be clickable
+
+4.Add New Student Button should add new student with dummy values and dummy values should be dynamically added e.g for row 4 the values will be Student 4, Teacher 4 etc. and for row 5 the values will be Student 5, Teacher 5 etc
+
+5.For newly added rows make sure existing CSS are applied too
+
+6.After successful add of the record display the pop-up message with newly added student name in the popup message e.g. if you have added row 4 for student 4 then display “Student 4 Record added successfully” and display error message in case add of record addition failed.
+
+
+7.On Selecting any of the checkboxes,
+
+- (1) Background color to be changed for the particular row to “Yellow” and 
+- (2) Submit button to turn orang
+- (3) A delete button should get added dynamically in DELETE column and should delete the entire row when clicked. Also, pop-up message should be displayed by having student name in the popup message e.g. if you have deleted row 4 for student 4 then display “Student 4 Record deleted successfully”
+- (4) An Edit button should get added dynamically in EDIT COLUMN and should only display pop up box with message e.g. if you are editing student 2 details then edit pop up should display as, title of Edit pop up should be “Edit details of Student 2” and it should display all the fields of student 2 row in the pop up details as text with “Update” and “Cancel” buttons. Data will not be editable as it is text format but when “Update” button is clicked then display “Student 2 data updated successfully” and when “Cancel” button is clicked pop up will be closed
+
+8.On Deselecting the checkbox will 
+
+- (1) make the row background color white again. 
+- (2) Also, if none of the rows are selected, the Submit button should be Grayed out again(disabled). (i.e., if 1 or more of the checkboxes are selected, the Submit button will be enabled and background color will be yellow. If none are selected, it will be disabled and background set to Gray)
+- (3) DELETE button in those de-selected rows will be hidden
+- (4) EDIT button in those de-selected rows will be hidden
+
+9.Clicking on any of the green arrows will expand the particular row. Clicking it again will collapse it (i.e., toggle view on click)
+
---
+++ week3_assmt_table/table.js
@@ -0,0 +1,268 @@
+window.onload = function(){
+    let authorInfo = document.querySelector('#authorInfo')
+    authorInfo.innerHTML = 'Yanqing Lou  002083406'
+    authorInfo.classList.add('authorInfo')
+
+    
+    // hide delete and edit columns
+    let trs = document.querySelectorAll('tr.row')
+    for(let i = 0; i < trs.length; i++){
+        if(trs[i].cells.length === 10 ){
+
+            trs[i].cells[8].style.display = 'none'
+            trs[i].cells[9].style.display = 'none'
+
+        }else if(trs[i].cells.length === 8 ){
+
+            trs[i].cells[8].style.display = 'none'
+        }     
+       
+    }
+
+    // close expanded row
+    let trsForDrop = document.querySelectorAll('.dropDownTextArea')
+    for(let i = 0; i<trsForDrop.length; i++){
+        trsForDrop[i].style.display = 'none'
+    }
+
+    // checkbox - unchecked
+    let inputs = document.querySelectorAll('input')
+    for(let i = 0; i<inputs.length; i++){ 
+        inputs[i].checked = false
+        inputs[i].parentNode.parentNode.style.backgroundColor = 'white'
+    }
+
+ 
+    // submit Button : disabled
+    document.querySelector('#button').disabled = true
+   
+
+    // setTimeout(function(){
+    //     authorInfo.style.display = 'none'
+    //     dispalyTable.style.display = 'block'
+    // },1000)
+
+}
+
+function showDetail(obj){
+    const tr = obj.parentElement.parentElement
+    const details = tr.nextElementSibling;
+    if(details.style.display == 'none'){
+        details.style.display = ''
+
+    }else{
+        details.style.display = 'none'
+    }
+    
+}
+
+function selectRow(obj){
+
+    let row = obj.parentElement.parentElement;
+    let submit = document.querySelector('#button')
+    let ths = document.querySelectorAll('.action')
+    let tds =  row.children 
+    let tdDel = row.lastElementChild.previousElementSibling
+    let tdEdi = row.lastElementChild
+    let btnEdi = tdEdi.children[0]
+    let btnDel = tdDel.children[0]
+
+    if(obj.checked){
+
+        row.style.backgroundColor = 'yellow'
+        ths[0].style.display = 'table-cell'
+        ths[1].style.display = 'table-cell'
+        tds[tds.length-1].style.display = 'table-cell'
+        tds[tds.length-2].style.display = 'table-cell'
+        btnEdi.style.display = 'table-cell'
+        btnDel.style.display = 'table-cell'
+        submit.disabled = false
+
+    }else if(!obj.checked){
+
+        row.style.backgroundColor = 'white'
+        tdDel.style.display = 'none'
+        tdEdi.style.display = 'none'
+
+        fresh() 
+    }
+}
+
+function fresh(){
+
+    let submit = document.querySelector('#button')
+    let ths = document.querySelectorAll('.action')
+
+    if(isCheckedInList()){
+
+        ths[0].style.display = 'table-cell'
+        ths[1].style.display = 'table-cell'
+        submit.disabled = false
+
+    }else if(!isCheckedInList()){
+
+        ths[0].style.display = 'none'
+        ths[1].style.display = 'none'
+        submit.disabled = true
+    } 
+}
+
+// There is checked row in the table
+function isCheckedInList(){
+    let inputs = document.querySelectorAll('input')
+    for(let i = 0; i<inputs.length; i++){ 
+        if(inputs[i].checked ){
+            return true
+        }else if(i === inputs.length-1){
+            return false
+        }
+    }
+}
+
+
+// Add Student
+function addStudent(){
+    let table = document.querySelector('#myTable')
+
+    let rows = document.querySelectorAll('tr.row')
+    let studentName = rows[rows.length-1].cells[1].innerHTML
+
+    let lastIndex = studentName.split(" ")[1]
+
+    // create tr /tds / innerHTML with input
+    let tr = document.createElement('tr')
+    tr.classList.add('row')
+    // create tds
+    let td1 = document.createElement('td')
+    td1.innerHTML = '<input onclick="selectRow(this)" type="checkbox" /><br /><br /><img onclick ="showDetail(this)" id="r1_img" src="down.png" width="25px" />'
+
+    let td2 = document.createElement('td')
+    td2.innerHTML = "Student " + (parseInt(lastIndex)+1)
+
+    let td3 = document.createElement('td')
+    td3.innerHTML = "Teacher " + (parseInt(lastIndex)+1)
+
+    let td4 = document.createElement('td')
+    td4.innerHTML = 'Approved'
+
+    let td5 = document.createElement('td')
+    td5.innerHTML = "Fall"
+
+    let td6 = document.createElement('td')
+    td6.innerHTML = "TA"
+    
+    let td7 = document.createElement('td')
+    td7.innerHTML = "12345"
+
+
+    let td8 = document.createElement('td')
+    td8.innerHTML = "100%"
+
+    let td9 = document.createElement('td')
+    td9.innerHTML = '<button onclick="removeRow(this)">Delete</button>'
+
+    let td10 = document.createElement('td')
+    td10.innerHTML= '<td><button onclick="edit(this)">Edit</button></td>'
+
+    //drop area
+    let trDrop = document.createElement('tr')
+    trDrop.innerHTML = '<td colspan="10">Advisor:<br /><br />Award Details<br />Summer 1-2014(TA)<br />Budget Number: <br />Tuition Number: <br />Comments:<br /><br /><br />Award Status:<br /><br /><br /></td>'
+    trDrop.style.display = 'none'
+    
+    tr.appendChild(td1)     
+    tr.appendChild(td2)     
+    tr.appendChild(td3)     
+    tr.appendChild(td4)     
+    tr.appendChild(td5)     
+    tr.appendChild(td6)     
+    tr.appendChild(td7)     
+    tr.appendChild(td8)     
+    tr.appendChild(td9)     
+    tr.appendChild(td10) 
+    
+    
+    table.appendChild(tr)
+    table.appendChild(trDrop)
+    td9.style.display = 'none'
+    td10.style.display = 'none'
+
+    let isSuccess = true;
+
+    if (isSuccess) {
+        alert(`${td2.innerHTML} Record added successfully!`);
+    } else {
+        alert("Error: Failed to add record!");
+    }
+    
+
+}
+
+
+// Delete: 'Student X Record deleted successfully'
+function removeRow(obj){
+
+    let table = document.querySelector('#myTable')
+    let row = obj.parentElement.parentElement
+    let dropBox = row.nextElementSibling
+    let student = row.children[1].innerHTML
+
+    if(row.parentElement.tagName === 'TBODY'){
+        document.querySelector('tbody').removeChild(row)
+        document.querySelector('tbody').removeChild(dropBox)
+    }else if(row.parentElement.tagName === 'TABLE'){
+        table.removeChild(row)
+        table.removeChild(dropBox)
+    } 
+
+    fresh()
+    alert( student + ' record deleted successfully!')  
+
+}
+
+// Edit : 'Edit details of Student X'
+function edit(obj){
+    let data = {};
+    let row = obj.parentElement.parentElement
+    let student = row.children[1].innerHTML
+    let advisor = row.children[2].innerHTML
+    let award = row.children[3].innerHTML
+    let semester = row.children[4].innerHTML
+    let type = row.children[5].innerHTML
+    let budget = row.children[6].innerHTML
+    let percentage = row.children[7].innerHTML
+
+    let modal = document.querySelector('#editModal')
+    // h4 : title
+    modal.firstElementChild.firstElementChild.innerHTML = `Edit details of ${student}`
+
+    data = {
+        Advisor:advisor,
+        Award:award,
+        Semester:semester,
+        Type:type,
+        Budget:budget,
+        Percentage:percentage
+    }
+    let div = document.querySelector('.text')
+    
+    // object type loop
+    Object.keys(data).forEach((key)=>{
+        div.innerHTML += `<p>${key}: ${data[key]}</p>`
+    });
+    modal.style.display = 'flex'
+
+}
+
+function update(obj){
+    let title = document.querySelector('div.modal-content>h4');
+    let student = title.innerHTML.split(' ')
+
+    document.querySelector('#editModal').style.display = 'none'
+    alert(`${student[3]} ${student[4]} data updated successfully!`)
+    document.querySelector('.text').innerHTML = ''
+}
+
+function cancel(){
+    document.querySelector('#editModal').style.display = 'none'
+    document.querySelector('.text').innerHTML = ''
+}
