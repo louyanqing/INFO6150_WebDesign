535eab6d3dc8c7d9fac38212166f42eedba24d68
louyanqing <lou.yan@northeastern.edu>
2024-10-31 03:13:24 -0400

    calculator-v1

---
+++ week7_assmt_calculator/calculator.js
@@ -0,0 +1,130 @@
+$(document).ready(function() {
+
+  // display username
+  let params = getParams(document.location.search);
+  $('#login-name').html(params['username'])
+
+  $('#addButton, #subtractButton, #multiplyButton, #divideButton').prop('disabled', true);
+  
+  const regExp = /[!@#$%^&*(),.?":{}|<>]/;
+
+  const validateNum = (numberField) => {
+    let isValid = true;
+    
+    // Number validation
+    const number = $(`#${numberField}`).val();
+    const err = $(`#${numberField}Err`)
+    if (!number) {
+      err.text("Please enter a number.");
+      isValid = false;
+    } else if (isNaN(number)) {
+      err.text("It is not a number, please enter a valid number.");
+      isValid = false;
+    } else if (regExp.test(number)) {
+      err.text("Special character are not allowed, please enter a valid number.");
+      isValid = false;
+    }else{
+      err.text("");
+    }
+
+    $('#addButton, #subtractButton, #multiplyButton, #divideButton').prop('disabled', !isValid);
+    $('#addButton, #subtractButton, #multiplyButton, #divideButton').toggleClass('disabled', !isValid);
+
+    return isValid;
+  };
+
+
+  const clearResult = () => {
+    $('#result').val("")
+  }
+
+  $('#number1').on('keyup', ()=>{
+    clearResult()
+    validateNum('number1')
+    validInputs()
+  })
+
+  $('#number2').on('keyup', ()=>{
+    clearResult()
+    validateNum('number2') 
+    validInputs()
+  })
+
+  const validInputs = () => {
+    const valid = ($(".error").text() === "" && $("#number1").val()!== "" && $("#number2").val()!== "")? true:false;
+    $('#addButton, #subtractButton, #multiplyButton, #divideButton').prop('disabled',!valid);
+    $('#addButton, #subtractButton, #multiplyButton, #divideButton').toggleClass('disabled', !valid);
+
+  }
+ 
+  // Single arrow function to handle all operations
+  const calculate = (operation) => {
+    if (!(validateNum('number1') && validateNum('number2'))) return;
+
+    const num1 = parseFloat($('#number1').val());
+    const num2 = parseFloat($('#number2').val());
+    let result;
+
+    switch (operation) {
+      case 'add':
+        result = num1 + num2;
+        break;
+      case 'subtract':
+        result = num1 - num2;
+        break;
+      case 'multiply':
+        result = num1 * num2;
+        break;
+      case 'divide':
+        if (num2 === 0) {
+          $('#number2Err').text("Cannot divide by zero.");
+          return;
+        }
+        result = num1 / num2;
+        break;
+      default:
+        result = "Invalid operation";
+    }
+
+    $('#result').val(result);
+  };
+
+
+  $('#addButton').on('click', () => calculate('add'));
+  $('#subtractButton').on('click', () => calculate('subtract'));
+  $('#multiplyButton').on('click', () => calculate('multiply'));
+  $('#divideButton').on('click', () => calculate('divide'));
+
+  $('#reset').on('click', () => {
+    $('#number1').val("")
+    $('#number2').val("")
+    $('#number1Err').val("")
+    $('#number2Err').val("")
+    $('#result').val("")
+    $('#addButton, #subtractButton, #multiplyButton, #divideButton').prop('disabled', true);
+  })
+  
+  $("#logout").on("click", () => {
+
+  });
+
+
+  // ====================
+  // Parse the params in the url
+  function getParams(url) {
+    url = url.split("+").join(" ");
+    var params = {},
+        tokens,
+        re = /[?&]?([^=]+)=([^&]*)/g;
+
+    while (tokens = re.exec(url)) {
+        params[decodeURIComponent(tokens[1])]
+            = decodeURIComponent(tokens[2]);
+    }
+
+    return params;
+  }
+
+
+
+});
---
+++ week7_assmt_calculator/main.js
@@ -0,0 +1,125 @@
+
+$(document).ready(function () {
+
+    clearForm()
+
+});
+
+var isCPTouched = false;
+var count = 0;
+
+const validateFunction = (field, regex, errorMsg, minLength, maxLength) => {
+    const errorElement = $('#error-' + field);
+    const inputElement = $('#' + field)
+    
+    // keyup event listener
+    inputElement.keyup( () => {
+
+        const value = inputElement.val()
+
+        const isConfirmPassword = (field === 'confirmPassword')? true:false;
+
+        if (isConfirmPassword) {
+            count++
+            isCPTouched = count>1;
+        }
+
+        PassWorChange(field);
+
+        if (value.length === 0) {
+            errorElement.text(inputElement.data('name') + ' cannot be empty!')
+            if(count<=1){
+                $('#error-confirmPassword').text('')
+            }
+            if(isConfirmPassword){
+                errorElement.text('Please confirm the password!')
+            }
+    
+        } else if (value.length < minLength || value.length > maxLength) {
+            if(!isConfirmPassword){
+                errorElement.text(`The length must be between ${minLength} and ${maxLength} characters`) 
+            } else if(isConfirmPassword && value.length !== minLength){
+                errorElement.text('Passwords do not match!')
+            }
+
+        } else if (isConfirmPassword && value !== $('#password').val() && isCPTouched) {
+            // If it's confirm password, check if it matches the password field
+            errorElement.text('Passwords do not match!')
+
+        }else if (regex && !regex.test(value)) {
+            errorElement.text(`${errorMsg}`);
+
+        } else {
+            errorElement.text('')
+        }
+        
+        checkFormValidity()
+
+    });
+}
+
+// Add Event Listener & RegExp    <- input
+validateFunction('username', /^[A-Za-z]+$/, 'User Name must contain letters only!', 2, 20);// {2,100}
+validateFunction('email', /^[a-zA-Z0-9._%+-]{1,64}@northeastern\.edu$/, 'Please enter a valid email address ending with @northeastern.edu!');
+validateFunction('password', /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,20}$/, 'Password must be at least one letter (uppercase or lowercase) and at least one digit!', 8, 20);
+validateFunction('confirmPassword', null, '', 8, 20);
+
+// Confirm password validation
+const PassWorChange = field => {
+    if(field === 'password'){
+        $('#confirmPassword').val('')
+        $('#error-confirmPassword').text('')
+        count = 0;
+
+    }       
+}
+
+const checkFormValidity = () => {
+    const isValid_inputs = checkInputsValidity()
+
+    if(!isValid_inputs){
+        $('#submitBtn').prop('disabled', true)
+        $('#submitBtn').removeClass('btn-primary')
+        $('#submitBtn').addClass('disabled-btn')   
+
+    }else{
+        $('#submitBtn').prop('disabled', false)
+        $('#submitBtn').removeClass('disabled-btn')
+        $('#submitBtn').addClass('btn-primary')
+
+    }
+}
+
+const checkInputsValidity = () => {
+    const inputs = $('.form-control')
+    const errors = $('.error')
+
+    const isEmpty = Array.from(inputs).some(input => $(input).val() == '');  
+    console.log(isEmpty);
+    const hasError = Array.from(errors).some(error => !$(error).is(':empty'));
+    console.log(hasError);
+
+
+    return (isEmpty||hasError)? false : true
+}
+
+function clearForm(){
+
+    // Disable the login button initially
+    $('#submitBtn').prop('disabled', true);
+    $('#submitBtn').addClass('disabled-btn');
+
+    $('#username').val('')
+    $('#email').val('')
+    $('#passname').val('')
+    $('#confirmPassword').val('')
+
+    $('#error-username').text('')
+    $('#error-email').text('')
+    $('#error-password').text('')
+    $('#error-confirmPassword').text('')       
+
+}
+
+
+
